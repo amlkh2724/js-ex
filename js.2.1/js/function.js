ac1b15312263704b8fe1c1ed32960010bfb3b19d
amal khanjer <amlkh2724@gmail.com>
2023-01-08 11:10:48 +0200

    country

---
+++ js.2.1/js/function.js
@@ -0,0 +1,11 @@
+
+function countryInfo(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+}
+var country1 = countryInfo('Spain', 47, 'Madrid');
+var country2 = countryInfo('paris', 47, 'Madrid');
+var country3 = countryInfo('israel', 47, 'jerusalem');
+
+console.log(country1);
+console.log(country2);
+console.log(country3)
