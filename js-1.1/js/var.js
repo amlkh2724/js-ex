a4b7f906af33d3e5e5da3bb7b259dbf054ab20fd
amal khanjer <amlkh2724@gmail.com>
2023-01-08 10:26:21 +0200

    js-begain

---
+++ js-1.1/js/var.js
@@ -0,0 +1,22 @@
+let myString = "hello";
+let number = 7
+let myCar = {
+    color: 'red',
+    wheels: 4,
+}
+let boolean = false
+
+let myNull = null
+
+let you
+
+
+const sym3 = Symbol("foo");
+
+console.log(typeof (myString));
+console.log(typeof (number));
+console.log(typeof (myCar));
+console.log(typeof (boolean));
+console.log(typeof (myNull));
+console.log(typeof (undefined));
+console.log(typeof (sym3));
