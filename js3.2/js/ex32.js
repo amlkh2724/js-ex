35712b5c1ffcb4024faeeb0c5e58e20208804a13
amal khanjer <amlkh2724@gmail.com>
2023-01-09 11:06:41 +0200

    functions

---
+++ js3.2/js/ex32.js
@@ -0,0 +1,33 @@
+// From function declarations to explicit and implicit
+// return functions (one of each)
+
+// function welcome() {
+// let welcome = 'Welcome to Appleseeds Bootcamp!';
+// return welcome;
+// }
+function welcome() {
+    return 'Welcome to Appleseeds Bootcamp!';
+}
+console.log(welcome());
+
+// function power(a) {
+// let myNumber = a;
+// let result = Math.pow(myNumber, 2);
+// return result;
+// }
+function power(a) {
+    return a * 2
+}
+console.log(power(2));
+// From function expressions to IIFE functions.
+// const squareRoot = a => Math.sqrt(a);
+
+((a) => {
+    return Math.sqrt(a)
+})(a)
+
+    // const randomNumbers = (a, b) => Math.random() * (a - b) +
+    //     b;
+    ((a, b) => {
+        return Math.random() * (a - b) + b
+    })(a)
