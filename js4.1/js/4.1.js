2f03dc74d909edcb92b49b09f3a493b733475b6a
amal khanjer <amlkh2724@gmail.com>
2023-01-09 14:01:40 +0200

    convertstringtonumber

---
+++ js4.1/js/4.1.js
@@ -0,0 +1,12 @@
+// 2
+
+let userChoice = +prompt("How many siblings do you have?")
+if (userChoice === 1) {
+    console.log("1 siblon");
+} else if (userChoice > 1) {
+    console.log("numSiblings is g1reater than1");
+} else {
+    console.log("No siblings");
+}
+
+// the type === we need and + convert string to number
