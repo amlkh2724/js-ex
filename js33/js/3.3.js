ae602d22f406357e9b567115dcb14438fef0e893
amal khanjer <amlkh2724@gmail.com>
2023-01-09 11:49:52 +0200

    ex3.3

---
+++ js33/js/3.3.js
@@ -0,0 +1,31 @@
+
+function percentageOfWorld1(population) {
+    // let value = prompt("Enter the value you wanted to return the population represents")
+    const value = 7900
+    let represents = (population / value) * 100
+    return represents
+}
+console.log(percentageOfWorld1(1441));
+let americaa = percentageOfWorld1(332)
+console.log(americaa);
+let france = percentageOfWorld1(67)
+console.log(france);
+let germany = percentageOfWorld1(83)
+console.log(germany);
+
+
+
+function percentageOfWorld2(population) {
+    // let value = prompt("Enter the value you wanted to return the population represents")
+    const value = 7900
+    let represents = (population / value) * 100
+    return represents
+}
+console.log(percentageOfWorld2(1441));
+let american = percentageOfWorld2(332)
+console.log(american);
+let france1 = percentageOfWorld2(67)
+console.log(france1);
+let germany2 = percentageOfWorld2(83)
+console.log(germany2);
+
