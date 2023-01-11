c71c02837a84243b50634513041f94cf76cf53a7
amal khanjer <amlkh2724@gmail.com>
2023-01-11 20:35:11 +0200

    swap values

---
+++ js8.5/js/8.5.js
@@ -0,0 +1,13 @@
+
+//  Create a function that receives 1 argument, an object, and
+//  returns a new object with the properties and values
+//  swapped.
+
+
+const swapped = (swapMe) => {
+    swap = {}
+    for (let swapp in swapMe) {
+        swap[swapMe[obj]] = swapp
+    }
+    return swapp
+}
