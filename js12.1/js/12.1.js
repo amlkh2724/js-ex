ffe48a900208b8845d77ac9c36694f35de7be92c
amal khanjer <amlkh2724@gmail.com>
2023-01-16 14:33:59 +0200

    isread

---
+++ js12.1/js/12.1.js
@@ -0,0 +1,37 @@
+// Write a JavaScript function that receives the following library
+// object as an input and displays the books that can be read (the
+// reading status is true).
+// Log the book name, author name, and reading status.
+
+
+
+const library = [
+    {
+        author: "Bill Gates",
+        title: "The Road Ahead",
+        readingStatus: true
+    },
+    {
+        author: "Steve Jobs",
+        title: "Walter Isaacson",
+        readingStatus: true
+    },
+    {
+        author: "Suzanne Collins",
+        title: "Mockingjay: The Final Book of The Hunger Games",
+        readingStatus: false
+    }
+];
+
+
+const isRead = library.filter(b => {
+    if (b.readingStatus) {
+        return {
+            author:b.author,
+            title:b.title,
+            readingStatus:b.readingStatus
+        }
+    }
+
+})
+console.log(isRead);
