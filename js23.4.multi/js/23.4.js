3445cff0a27597f1ff42c996ef93db61c689921c
amal khanjer <amlkh2724@gmail.com>
2023-01-30 21:00:00 +0200

    table

---
+++ js23.4.multi/js/23.4.js
@@ -0,0 +1,11 @@
+
+const table = document.querySelector("#table tbody");
+let output = "";
+for (let i = 1; i <= 10; i++) {
+    output += "<tr>";
+    for (let j = 1; j <= 20; j++) {
+        output += "<td>" + i * j + "</td>";
+    }
+    output += "</tr>";
+}
+table.innerHTML = output;
