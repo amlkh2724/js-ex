eb03ed00a9161534d2c2106a4667a171a6c46a33
amal khanjer <amlkh2724@gmail.com>
2023-02-06 20:05:45 +0200

    29.1

--- js29.1/js/29.1.js
+++ js29.1/js/29.1.js
@@ -1,22 +1,36 @@
-// // We are faking an API request to a food recipe database.
-// // We get an array of Ids once consumed.
 
-// // Lets change the code below to use async await instead of .then
-// // and .catch
+// Lets change the code below to use async await instead of .then
+// and .catch
+// Understanding the code
+// We are faking an API request to a food recipe database.
+// We get an array of Ids once consumed.
 const getIDs = () =>
-new Promise((resolve, reject) => {
-setTimeout(() => {
-resolve([532, 543, 753, 1, 5]);
-}, 1500);
-});
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([532, 543, 753, 1, 5]);
+        }, 1500);
+    });
+// Here we are fetching a recipe with a given Id.
+// We are getting a recipe as an object once consumed.
+const getRecipe = (recipeID) => {
+    return new Promise((resolve, reject) => {
+        setTimeout((ID) => {
+            const recipe = {
+                title: "Fresh tomato pasta",
+                publisher: "Pinchas Hodadad",
+            };
+            resolve(`${ID}: ${recipe.title}`);
+        }, 1500, recipeID);
+    });
+};
 
-
-async function getID() {
+async function getRecipeDetails() {
     try {
       const IDs = await getIDs();
-      console.log(IDs);
+      const recipe = await getRecipe(IDs[1]);
+      console.log(recipe);
     } catch (error) {
       console.error(error);
     }
   }
-  getID()
+console.log(getRecipeDetails());
